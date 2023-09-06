1ea94e4d4edeae1d9c8d45e635291d42bb545d19
platon <platon@enod.xyz>
2023-09-06 11:32:31 +0500

    KS-01-2 -- reassigning variables

---
+++ src/main/kotlin/lesson_01/lesson_01-task_2.kt
@@ -0,0 +1,16 @@
+package lesson_01
+
+/** Created by Platon2025 */
+
+fun main(){
+    val numberOfOrders: Int = 75
+    val thankYouMessage: String = "Спасибо за покупку! Обращайся, если что."
+    println(numberOfOrders)
+    println(thankYouMessage)
+
+    var numberOfEmployees: Int = 2000
+//    println(numberOfEmployees)
+    numberOfEmployees = 1999
+    println(numberOfEmployees)
+
+}
