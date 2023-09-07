de835a6129668cfa9e6814924ef738f5ed86da62
platon <platon@enod.xyz>
2023-09-07 14:37:25 +0500

    KS-02-3 -- Train arrival time calculation

--- src/main/kotlin/lesson_02/lesson_02-task_3.kt
+++ src/main/kotlin/lesson_02/lesson_02-task_3.kt
@@ -2,3 +2,16 @@ package lesson_02
 
 /** Created by Platon2025 */
 
+fun main() {
+    val departureHour = 9
+    val departureMinute = 39
+    val travelMinutes = 457
+
+    val arriveMinutesFrom00 = departureHour * 60 + departureMinute + travelMinutes
+    val arriveMinutes = arriveMinutesFrom00 % 60
+    val arriveHours = arriveMinutesFrom00 / 60 % 24
+
+    val arriveTime = String.format("%1d:%02d", arriveHours, arriveMinutes)
+    println("Время прибытия: $arriveTime")
+
+}
