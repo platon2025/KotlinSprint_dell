8e5d19616244d6c56ea380a73ffeb59c7f0cf7dc
platon <platon@enod.xyz>
2023-09-07 08:17:17 +0500

    KS-01-3 -- print, println output

---
+++ src/main/kotlin/lesson_01/lesson_01-task_3.kt
@@ -0,0 +1,19 @@
+package lesson_01
+
+/** Created by Platon2025 */
+
+fun main() {
+    val year = 1961
+    var hour = 9
+    var minute = 7
+    println(year)
+    println(hour)
+    println(minute)
+
+    hour = 10
+    minute = 55
+    print (hour)
+    print(':')
+    print(minute)
+
+}
